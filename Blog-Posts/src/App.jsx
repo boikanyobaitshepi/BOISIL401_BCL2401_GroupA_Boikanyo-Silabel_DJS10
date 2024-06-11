3126c06238f51507ddb69de13a11e72e40504f18
boikanyobaitshepi <boikanyobaitshepi@gmail.com>
2024-06-11 14:24:20 +0200

    IMPORTED THE FILES ON APP.JSX
    ADDED A CSS

--- Blog-Posts/src/App.jsx
+++ Blog-Posts/src/App.jsx
@@ -1,14 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import "./App.css"
+import images from './images/error-message.png'
 
 function App() {
-  //const [count, setCount] = useState(0)
- // const [name, setName] = useState('')
-  //const [age, setAge] = useState(0)
-  //const [users, setUsers] = useState([
-   // { id: 1, name: 'John Doe', age: 30 },
-    //{ id: 2, name: 'Jane Doe', age: 25 },
-    //])
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        if (!response.ok) {
+          throw new Error('Data fetcing Failed');
+        }
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+        setError(error.message);
+      }
+    }
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Posts</h1>
+      {error ? (
+        <div className="error-message">
+          <img src={images} alt="" />
+        </div>
+      ) : (
+        <div className="post-list">
+          {posts.map(post => (
+            <div key={post.id} className="post">
+              <h2>{post.id}. {post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
+
+export default App;
+
